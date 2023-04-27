04dd74bd9ebfee257cbf8a223d1782b11de197c8
jeonghwan <ajh5251@gmail.com>
2023-04-27 16:06:44 +0900

    Update views index, detail 추가

--- plates/urls.py
+++ plates/urls.py
@@ -6,4 +6,9 @@ from . import views
 app_name = 'plates'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:post_pk>/', views.detail, name='detail'),
+    path('<int:post_pk>/delete/', views.delete, name='delete'),
+    path('<int:post_pk_pk>/update/', views.update, name='update'),
+
 ]
