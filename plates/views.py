b810d79e593e527fdcb6a5761722cc9201dfa9c0
j1-y00n <byunjiyun@naver.com>
2023-04-28 14:23:01 +0900

    index-template

--- plates/views.py
+++ plates/views.py
@@ -32,7 +32,7 @@ def detail(request, post_pk):
 # @login_required
 def create(request):
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user
