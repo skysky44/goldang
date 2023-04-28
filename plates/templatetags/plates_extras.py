1eeba534b22da29ee499c31408c8f084261eb8e9
thebjko <thebjko@gmail.com>
2023-04-28 10:56:16 +0900

    커스텀 필터 구현 완료

--- plates/templatetags/plates_extras.py
+++ plates/templatetags/plates_extras.py
@@ -1,7 +1,8 @@
 from django import template
-
+from django.db.models import Avg
 register = template.Library()
 
-
+@register.filter
 def get_rating(obj):
-    return obj.review_set.aggregate(Avg('rating')).get('rating__avg')
+    rating = obj.review_set.aggregate(Avg('rating')).get('rating__avg')
+    return round(rating, 1)
