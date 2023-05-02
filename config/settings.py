e0109bd86de83652b4f0b3dc28ad0eb3dd4a466c
thebjko <thebjko@gmail.com>
2023-05-02 11:11:40 +0900

    edit STATIC

--- config/settings.py
+++ config/settings.py
@@ -126,10 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    '/home/ubuntu/goldang/static',
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+        # '/home/ubuntu/goldang/static',
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
