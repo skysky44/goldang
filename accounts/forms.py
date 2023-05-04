66ca226df671d6b42e465503e6bd05f6c7fe02f6
jeonghwan <ajh5251@gmail.com>
2023-05-05 00:21:36 +0900

    로그인, 회원정보수정, 비밀번호 변경 화면

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,157 +1,185 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField, AuthenticationForm,  PasswordResetForm
-from django.contrib.auth import get_user_model, password_validation
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    AuthenticationForm,
+    PasswordChangeForm,
+)
+
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-from django.forms import CharField, PasswordInput
-from django.core.validators import RegexValidator
 
 
 class CustomUserCreationForm(UserCreationForm):
-    address = forms.CharField(label='주소', required=False)
-    password1 = forms.CharField(
-        label=_("비밀번호"),
-        label_suffix='',
-        strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label=_("비밀번호 확인"),
-        label_suffix='',
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-        strip=False,
-        help_text=_("비밀번호를 한번 더 입력하세요"),
-    )
-
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
-        fields = (
-            'username',
-            'nickname',
-            'email',
-            'picture',
-            'address',
-            'password1',
-            'password2',
-        )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.fields['username'].label = '아이디'
-        self.fields['nickname'].label = '닉네임'
-        self.fields['email'].label = '이메일'
-        self.fields['picture'].label = '프로필 사진'
-        self.fields['address'].label = '주소'
-
-        self.fields['username'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': '아이디',
-            'aria-label': 'Username',
-            'aria-describedby': 'username-addon'
-        })
-        self.fields['nickname'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': '닉네임',
-            'aria-label': 'Nickname',
-            'aria-describedby': 'nickname-addon'
-        })
-        self.fields['email'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': '이메일',
-            'aria-label': 'Email',
-            'aria-describedby': 'email-addon'
-        })
-        self.fields['picture'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': '프로필 사진',
-            'aria-label': 'Picture',
-            'aria-describedby': 'picture-addon'
-        })
-        self.fields['address'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': '주소',
-            'aria-label': 'Address',
-            'aria-describedby': 'address-addon'
-        })
-
-
-class CustomUserCreationForm(UserCreationForm):
-    address = forms.CharField(label='주소', required=False)
-    nickname = forms.CharField(label='닉네임', max_length=30,
-                               validators=[RegexValidator(r'^[\w.@+-]+$', _('닉네임은 30자 이내의 알파벳, 숫자, 밑줄, @, ., +, - 문자만 가능합니다.'))])
-    picture = forms.ImageField(label='프로필 사진', required=False,
-                                help_text='2MB 이하의 jpg, jpeg, png, gif 이미지 파일을 업로드해주세요.')
-
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()
-        fields = (
-            'username',
-            'nickname',
-            'email',
-            'picture',
-            'address',
-            'password1',
-            'password2',
-        )
-
-
-class CustomPasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(
-        label=_("이메일 주소"),
-        label_suffix='',
-        max_length=254,
-        widget=forms.EmailInput(attrs={
-            'autocomplete': 'email',
-            'class': 'form-control',
-            'placeholder': '이메일 주소'
-        }),
-        help_text=_("암호 재설정 링크를 받을 이메일 주소를 입력하세요."),
-    )
-
-class CustomUserChangeForm(UserChangeForm):
-    first_name = forms.CharField(label=_("이름"), label_suffix='')
-    last_name = forms.CharField(label=_("성"), label_suffix='')
-    email = forms.EmailField(label=_("이메일"), label_suffix='')
-    password = ReadOnlyPasswordHashField(
-        label='',
-        label_suffix='',
-        help_text=_(
-            '<a href="{}">비밀번호 변경하기</a>'
+    username = forms.CharField(
+        label="아이디",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "아이디를 입력하세요",
+                "style": "width: 400px; height: 50px;",
+            }
         ),
     )
-    class Meta(UserChangeForm.Meta):
-        model = get_user_model()
-        fields = (
-            'nickname',
-            'email',
-            'picture',
-            
-        )
+    email = forms.EmailField(
+        label="이메일",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "이메일을 입력하세요",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+    )
 
-# class CustomAuthenticationForm(AuthenticationForm):
-#     username = forms.CharField(label='아이디',
-#                                widget = forms.TextInput(attrs=
-#                                                         class="form-control",
-#                                                           placeholder="아이디",
-#                                                             aria-label="Username" aria' 
+    password1 = forms.CharField(
+        label="비밀번호",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "비밀번호를 입력하세요",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        label="비밀번호 확인",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "비밀번호를 다시 입력하세요",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ("username", "email", "password1", "password2")
+
+
+# class CustomPasswordResetForm(PasswordResetForm):
+#     email = forms.EmailField(
+#         label=_("이메일 주소"),
+#         label_suffix="",
+#         max_length=254,
+#         widget=forms.EmailInput(
+#             attrs={
+#                 "autocomplete": "email",
+#                 "class": "form-control",
+#                 "placeholder": "이메일 주소",
+#             }
+#         ),
+#         help_text=_("암호 재설정 링크를 받을 이메일 주소를 입력하세요."),
 #     )
 
 
+class CustomUserChangeForm(UserChangeForm):
+    email = forms.EmailField(
+        label="이메일",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "이메일을 입력하세요",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+    )
+    nickname = forms.CharField(
+        label="닉네임",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "닉네임을 입력하세요",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+    )
+    picture = forms.ImageField(
+        label="사진",
+        widget=forms.FileInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "사진을 선택하세요",
+                "style": "width: 400px;",
+            }
+        ),
+    )
+    address = forms.CharField(
+        label="주소",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "주소를 입력하세요",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+    )
+
+    password = None
+
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
+        fields = ("email", "nickname")
+
+
 class CustomAuthenticationForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].label = '아이디'
-        self.fields['username'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': '아이디',
-            'aria-label': 'Username',
-            'aria-describedby': 'username-addon'
-        })
-        self.fields['password'].label = '비밀번호'
-        self.fields['password'].widget = PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': '비밀번호',
-            'aria-label': 'Password',
-            'aria-describedby': 'password-addon'
-        })
+    username = forms.CharField(
+        label="아이디",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "아이디를 입력하세요",
+                "style": "width: 400px; height: 50px;",
+                "autocomplete": "username",
+                #'style' : 'width: 400px;'
+            }
+        ),
+    )
+    password = forms.CharField(
+        label="비밀번호",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "비밀번호를 입력하세요",
+                "style": "width: 400px; height: 50px;",
+                "autocomplete": "current-password",
+                #'style' : 'width: 400px;'
+            }
+        ),
+    )
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "기존 비밀번호",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+    )
+    new_password1 = forms.CharField(
+        label=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "새 비밀번호",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+        help_text="",
+    )
+    new_password2 = forms.CharField(
+        label=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "새 비밀번호 확인",
+                "style": "width: 400px; height: 50px;",
+            }
+        ),
+        help_text="",
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -2,98 +2,105 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import update_session_auth_hash, get_user_model
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, CustomUserChangeForm, CustomAuthenticationForm
+from .forms import (
+    CustomUserCreationForm,
+    CustomUserChangeForm,
+    CustomAuthenticationForm,
+    CustomPasswordChangeForm,
+)
 from django.http import JsonResponse
+
 
 # Create your views here.
 def signup(request):
     if request.user.is_authenticated:
-        return redirect('plates:index')
+        return redirect("plates:index")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('plates:index')
+            return redirect("plates:index")
     else:
         form = CustomUserCreationForm()
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'accounts/signup.html', context)
+    return render(request, "accounts/signup.html", context)
 
 
 def login(request):
     if request.user.is_authenticated:
-        return redirect('plates:index')
+        return redirect("plates:index")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('plates:index')
+            return redirect("plates:index")
     else:
         form = CustomAuthenticationForm()
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'accounts/login.html', context)
+    return render(request, "accounts/login.html", context)
 
 
 @login_required
 def logout(request):
     auth_logout(request)
-    return redirect('plates:index')
+    return redirect("plates:index")
 
 
 @login_required
 def delete(request):
     request.user.delete()
     auth_logout(request)
-    return redirect('plates:index')
+    return redirect("plates:index")
 
 
 @login_required
 def update(request):
-    if request.method == 'POST':
-        form = CustomUserChangeForm(request.POST, instance=request.user, files=request.FILES)
+    if request.method == "POST":
+        form = CustomUserChangeForm(
+            request.POST, instance=request.user, files=request.FILES
+        )
         if form.is_valid():
             form.save()
-            return redirect('plates:index')
+            return redirect("plates:index")
     else:
         form = CustomUserChangeForm(instance=request.user)
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'accounts/update.html', context)
+    return render(request, "accounts/update.html", context)
 
 
 @login_required
 def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(user=request.user, data=request.POST)
+    if request.method == "POST":
+        form = CustomPasswordChangeForm(user=request.user, data=request.POST)
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)
-            return redirect('plates:index')
+            return redirect("plates:index")
     else:
-        form = PasswordChangeForm(request.user)
+        form = CustomPasswordChangeForm(request.user)
     context = {
-        'form': form,
+        "form": form,
     }
-    return render(request, 'accounts/change_password.html', context)
+    return render(request, "accounts/change_password.html", context)
 
 
 @login_required
 def profile(request, username):
     User = get_user_model()
     context = {
-        'person': User.objects.get(username=username),
+        "person": User.objects.get(username=username),
     }
-    return render(request, 'accounts/profile.html', context)
+    return render(request, "accounts/profile.html", context)
 
 
 @login_required
@@ -110,9 +117,9 @@ def follow(request, user_pk):
             user.followers.add(me)
             is_followed = True
         context = {
-            'is_followed': is_followed,
-            'followings_count': user.followings.count(),
-            'followers_count': user.followers.count(),
+            "is_followed": is_followed,
+            "followings_count": user.followings.count(),
+            "followers_count": user.followers.count(),
         }
         return JsonResponse(context)
-    return redirect('accounts:profile', user.username)
+    return redirect("accounts:profile", user.username)
