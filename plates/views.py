47498a27e531845f917ec00e46354e64a842634e
jeonghwan <ajh5251@gmail.com>
2023-05-07 01:10:42 +0900

    view 필터 오류 수정

--- plates/views.py
+++ plates/views.py
@@ -8,7 +8,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-    지역별_맛집 = Post.objects.filter(address_city="서울시 마포구" or "서울특별시 마포구")[:8]
+    지역별_맛집 = Post.objects.filter(address_city__in=["서울시 마포구", "서울특별시 마포구"])[:8]
     조회수_맛집 = Post.objects.order_by("-visited")[:8]
 
     context = {
@@ -171,7 +171,7 @@ def review_update(request, post_pk, review_pk):
 
 # @login_required
 def review_delete(request, post_pk, review_pk):
-    review = Review.objects.get(pk=review_pk)
+    review = review.objects.get(pk=review_pk)
 
     if request.user == review.user:
         review.delete()
