1a0ba4cac8fa63767f901400b23ce5ef8c2b1620
seho1278 <seho1278@naver.com>
2023-05-06 22:24:21 +0900

    디테일 페이지, 리뷰 수정 페이지 완성 및 코드정리

--- plates/views.py
+++ plates/views.py
@@ -8,17 +8,16 @@ from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-    지역별_맛집 = Post.objects.filter(address_city="서울시 마포구" or "서울특별시 마포구")[:8]
-    조회수_맛집 = Post.objects.order_by("-visited")[:8]
-
+    지역별_맛집 = Post.objects.filter(address_city='서울시 마포구')[:8]
+    조회수_맛집  = Post.objects.order_by('-visited')[:8]
     context = {
-        "posts": posts,
-        "지역별_맛집": 지역별_맛집,
-        "조회수_맛집": 조회수_맛집,
+        'posts': posts,
+        '지역별_맛집': 지역별_맛집,
+        '조회수_맛집': 조회수_맛집,
     }
-    return render(request, "plates/index.html", context)
+    return render(request, 'plates/index.html', context)
 
-
+# 수정
 def detail(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     posts = Post.objects.all()
@@ -26,56 +25,43 @@ def detail(request, post_pk):
     comment_form = CommentForm()
     reviews = post.review_set.all()
 
-    # 템플릿 페이징
-    taste_good = post.review_set.filter(taste_evaluation=5)
-    taste_okey = post.review_set.filter(taste_evaluation=3)
-    taste_bad = post.review_set.filter(taste_evaluation=1)
-
-    items_per_page = 5
-    paginator = Paginator(reviews, items_per_page)
-    taste_good1 = Paginator(taste_good, items_per_page)
-    taste_okey1 = Paginator(taste_okey, items_per_page)
-    taste_bad1 = Paginator(taste_bad, items_per_page)
-
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    num_pages = paginator.num_pages
-    num_pages1 = taste_good1.num_pages if taste_good1.num_pages > 0 else 1
-    num_pages2 = taste_okey1.num_pages if taste_okey1.num_pages > 0 else 1
-    num_pages3 = taste_bad1.num_pages if taste_bad1.num_pages > 0 else 1
-
+    print(reviews)
+    review_images = []
     post_images = PostImage.objects.filter(post=post)
-    nearby_restaurants = posts.filter(address_city=post.address_city).exclude(
-        pk=post_pk
-    )
+    for review in reviews:
+        for review_image in review.review_images.all():
+            review_images.append(review_image)
+
+    review_images_reversed = list(reversed(review_images))
+    
+
+    nearby_restaurants = posts.filter(address_city=post.address_city).exclude(pk=post_pk)[:5]
     post.visited += 1
     post.save()
 
     context = {
-        "post": post,
-        "post_images": post_images,
-        "review_form": review_form,
-        "reviews": reviews,
-        "comment_form": comment_form,
-        "맛있다": taste_good,
-        "괜찮다": taste_okey,
-        "별로": taste_bad,
-        "nearby_restaurants": nearby_restaurants,
-        # 페이지네이션 context
-        "page_obj": page_obj,
-        "num_pages": num_pages,
-        "num_pages1": num_pages1,
-        "num_pages2": num_pages2,
-        "num_pages3": num_pages3,
+        'post': post,
+        'post_images': post_images,
+        'review_form': review_form,
+        'reviews': reviews,
+        'comment_form': comment_form,
+        '맛있다': post.review_set.filter(taste_evaluation=5),
+        '괜찮다': post.review_set.filter(taste_evaluation=3),
+        '별로': post.review_set.filter(taste_evaluation=1),
+        'review_images': review_images,
+        'review_images_reversed': review_images_reversed,
+
+        'nearby_restaurants': nearby_restaurants,
     }
 
-    return render(request, "plates/detail.html", context)
+    return render(request, 'plates/detail.html', context)
+
+
 
 
 # @login_required
 def create(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         imageForm = PostImageForm(request.POST, request.FILES)
         if form.is_valid() and imageForm.is_valid():
@@ -83,17 +69,17 @@ def create(request):
             post.user = request.user
             post.save()
 
-            for image in request.FILES.getlist("image"):  # s 없나
+            for image in request.FILES.getlist('image'): # s 없나
                 PostImage.objects.create(post=post, image=image)
-            return redirect("plates:detail", post.pk)
+            return redirect('plates:detail', post.pk)
     else:
         form = PostForm()
         imageForm = PostImageForm()
     context = {
-        "form": form,
-        "imageForm": imageForm,
+        'form': form,
+        'imageForm': imageForm,
     }
-    return render(request, "plates/create.html", context)
+    return render(request, 'plates/create.html', context)
 
 
 # @login_required
@@ -101,35 +87,37 @@ def delete(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     if request.user == post.user:
         post.delete()
-    return redirect("plates:index")
+    return redirect('plates:index')
 
 
 # @login_required
 def update(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     if request.user == post.user:
-        if request.method == "POST":
+        if request.method == 'POST':
             form = PostForm(request.POST, instance=post)
             if form.is_valid():
                 form.save()
-                return redirect("plates:detail", post.pk)
+                return redirect('plates:detail', post.pk)
         else:
             form = PostForm(instance=post)
     else:
-        return redirect("plates:index")
+        return redirect('plates:index')
     context = {
-        "post": post,
-        "form": form,
+        'post': post,
+        'form': form,
     }
-    return render(request, "plates/update.html", context)
+    return render(request, 'plates/update.html', context)
+
 
 
 def review_create(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     post_form = PostForm(request.POST)
-
+    
+    
     print(post, post_form)
-    if request.method == "POST":
+    if request.method == 'POST':
         review_form = ReviewForm(request.POST)
         imageForm = ReviewImageForm(request.POST, request.FILES)
         if review_form.is_valid() and imageForm.is_valid():
@@ -139,34 +127,55 @@ def review_create(request, post_pk):
             review.save()
             print(review.save())
 
-            for image in request.FILES.getlist("image"):
+            for image in request.FILES.getlist('image'):
                 ReviewImage.objects.create(review=review, image=image)
 
-            return redirect("plates:detail", post.pk)
+            return redirect('plates:detail', post.pk)
     else:
         review_form = ReviewForm()
         imageForm = ReviewImageForm()
     context = {
-        "post": post,
+        'post': post,
         # 'post_form': post_form,
-        "review_form": review_form,
-        "imageForm": imageForm,
+        'review_form': review_form,
+        'imageForm': imageForm,
     }
-    return render(request, "plates/review.html", context)
+    return render(request, 'plates/review.html', context)
 
 
 # @login_required
+
+# 수정
 def review_update(request, post_pk, review_pk):
     post = Post.objects.get(pk=post_pk)
-    review = Review.objects.get(pk=review_pk)
-    if request.user == review:
-        if request.method == "POST":
-            form = ReviewForm(request.POST, instance=review)
-            if form.is_valid():
-                form.save()
-                return redirect("plates:detail", post.pk)
+    review = Review.objects.get(pk=review_pk, post=post)
+    if request.user == review.user:
+        if request.method == 'POST':
+            review_form = ReviewForm(request.POST, instance=review)
+            imageForm = ReviewImageForm(request.POST, request.FILES, instance=review.review_images.first())
+            if review_form.is_valid() and imageForm.is_valid():
+                review_form.save()
+
+                ReviewImage.objects.filter(review=review).delete()
+                for image in request.FILES.getlist('image'):
+                    ReviewImage.objects.create(review=review, image=image)
+
+                return redirect('plates:detail', post.pk)
         else:
-            form = ReviewForm(instance=review)
+            review_form = ReviewForm(instance=review)
+            imageForm = ReviewImageForm(instance=review.review_images.first())
+
+    else:
+        return redirect('plates:detail', post.pk)
+
+    context = {
+        'review_form': review_form,
+        'post': post,
+        'review': review,
+        'imageForm': imageForm,
+    }
+
+    return render(request, 'plates/review_update.html', context)
 
 
 # @login_required
@@ -175,7 +184,7 @@ def review_delete(request, post_pk, review_pk):
 
     if request.user == review.user:
         review.delete()
-    return redirect("plates:detail", post_pk)
+    return redirect('plates:detail', post_pk)
 
 
 def likes(request, post_pk):
@@ -185,13 +194,12 @@ def likes(request, post_pk):
         post.like_users.remove(request.user)
     else:
         post.like_users.add(request.user)
-    return redirect("plates:detail", post_pk)
-
+    return redirect('plates:detail', post_pk)
 
 def comment_create(request, post_pk, review_pk):
     post = Post.objects.get(pk=post_pk)
     review = Review.objects.get(pk=review_pk)
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
@@ -199,35 +207,44 @@ def comment_create(request, post_pk, review_pk):
             comment.review = review
             comment.user = request.user
             comment.save()
-            return redirect("plates:detail", post.pk)
-
+            return redirect('plates:detail', post.pk)
 
 def comment_update(request, post_pk, review_pk, comment_pk):
     post = Post.objects.get(pk=post_pk)
     review = Review.objects.get(pk=review_pk)
     comment = Comment.objects.get(pk=comment_pk)
     if request.user == comment.user:
-        if request.method == "POST":
+        if request.method == 'POST':
             form = CommentForm(request.POST, instance=comment)
             if form.is_valid():
                 form.save()
-                return redirect("plates:detail", post.pk)
+                return redirect('plates:detail', post.pk)
         else:
             form = CommentForm(instance=comment)
-
 
 def comment_delete(request, post_pk, review_pk, comment_pk):
     comment = comment.objects.get(pk=comment_pk)
     if request.user == comment.user:
         comment.delete()
-    return redirect("plates:detail", post_pk)
+    return redirect('plates:detail', post_pk)
+
+
+
+
+
+
+
+
+
+
+
 
 
 def review_detail(request, post_pk, review_pk):
     review = Review.objects.get(pk=review_pk)
     review_images = ReviewImage.objects.filter(review=review)
     context = {
-        "review": review,
-        "review_images": review_images,
+        'review': review,
+        'review_images': review_images,
     }
-    return render(request, "plates/review_detail.html", context)
+    return render(request, 'plates/review_detail.html', context)
